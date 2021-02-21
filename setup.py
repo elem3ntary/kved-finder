65f8a99f1431f1f4295e587da9fdf4e4278309c2
Pr0digg <impr0digg@gmail.com>
2021-02-21 13:58:24 +0200

    metadata added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kved_finder-elem3ntary",  # Replace with your own username
+    version="0.0.1",
+    author="elem3ntary",
+    author_email="impr0digg@gmai.com",
+    description="A package for finding kved",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elem3ntary/kved_finder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
