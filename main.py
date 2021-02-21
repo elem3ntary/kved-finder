736dc44aca584e5c2fbf536dc74a7d309643abef
Pr0digg <impr0digg@gmail.com>
2021-02-21 13:12:09 +0200

    Rewriten search, added argument parser

--- main.py
+++ main.py
@@ -1,75 +1,76 @@
 import json
-import pprint
-import functools.
+import argparse
 
 
-def read_file(file_path='kved.json'):
+def read_file(file_path='kved.json') -> dict:
+    '''
+    Reads json file and returns it parsed
+    '''
     with open(file_path) as f:
-        return json.loads(f.read())
-
-
-def get_section_and_division(kved_obj, divisionCode):
-    for section in kved_obj['sections'][0]:
-        print(section)
-        for division in section['divisions']:
-            if division['divisionCode'] == divisionCode:
-                return section, division
-
-
-def get_group(groups, groupCode):
-    for group in groups:
-        if group['groupCode'] == groupCode:
-            return group
-
-
-def get_class(classes, classCode):
-    for item in classes:
-        if item['classCode'] == classCode:
-            return item
+        return json.load(f)
 
 
 def find(func, values):
-    for i in values:
-        if func(i):
-            return i
+    '''
+    Returns first value in values that matches a criterion
+    '''
+    for value in values:
+        if func(value):
+            return value
 
 
 def parse_kved(classCode: str):
-    divisionCode = classCode.split()[0]
+    '''
+    Parses kved tree and writes results to kved_result.json
+    '''
+    divisionCode = classCode.split('.')[0]
     groupCode = classCode[:-1]
 
-    # value = find(lambda x: x['classCode'] == classCode, list1)
-
     kved_obj = read_file()
-    value = (i break
-             if i == classCode for i in kvedobj)
-    kved_section = get_section_and_division(kved_obj, divisionCode)
-    print(kved_section)
-    kved_group = get_group(kved_division['groups'], groupCode)
-    kved_class = get_class(kved_group['classes'], classCode)
+    for section in kved_obj['sections'][0]:
+        divisions = section['divisions']
+        division = find(lambda i: i['divisionCode'] == divisionCode, divisions)
 
-    data = [
-        ('section', kved_section['sectionName'], len(kved_section['divisions'])),
-        ('division', kved_division['divisionName'], len(kved_division['groups'])),
-        ('group', kved_group['groupName'], len(kved_group['classes'])),
-        ('class', kved_class['className'])
-    ]
-    block = None
-    start_block = dict()
-    for idx, item in enumerate(reversed(data)):
-        if idx == 0:
-            start_block = {
-                'type': item[0],
-                'name': item[1]
+        if not division:
+            continue
+
+        groups = division['groups']
+        group = find(lambda i: i['groupCode'] == groupCode, groups)
+
+        if not group:
+            continue
+
+        classes = group['classes']
+        class_name = find(lambda i: i['classCode'] == classCode, classes)['className']
+
+        if class_name:
+            break
+
+    result = {
+        "name": class_name,
+        "type": "class",
+        "parent": {
+            "name": group['groupName'],
+            "type": "group",
+            "num_children": len(group['classes']),
+            "parent": {
+                "name": division['divisionName'],
+                "type": "division",
+                "num_children": len(division['groups']),
+                "parent": {
+                    "name": section['sectionName'],
+                    "type": "section",
+                    "num_children": len(section['divisions'])
+                }
             }
-            block = start_block
-        if item[2]:
-            block['num_children'] = item[2]
-        if idx != len(data) - 1:
-            block['parent'] = dict()
-            block = block['parent']
-    print(start_block)
+        }
+    }
+    with open('kved_result.json', 'w') as file:
+        json.dump(result, file, indent=2, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    parse_kved('01.10')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('kved', help='kved you want to find (example: 01.11)', type=str)
+    args = parser.parse_args()
+    parse_kved(args.kved)
