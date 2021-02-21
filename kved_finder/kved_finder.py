a87f7ae515cce161edc67a0daffd47d819a2db99
Pr0digg <impr0digg@gmail.com>
2021-02-21 16:46:49 +0200

    Automatic kved loading

--- kved_finder/kved_finder.py
+++ kved_finder/kved_finder.py
@@ -1,15 +1,21 @@
 import json
 import argparse
-import os.path
+import requests
 
 
-def read_file(file_path='static/kved.json') -> dict:
+def read_file(file_path) -> dict:
     '''
     Reads json file and returns it parsed
     '''
-    current_dir = os.path.dirname(__file__)
-    with open(os.path.join(current_dir, file_path)) as f:
+    with open(file_path) as f:
         return json.load(f)
+
+
+def get_kved_data():
+    response = requests.request(
+        'GET',
+        url='https://data.gov.ua/dataset/f8a741b9-af17-48e2-8178-8e161c244549/resource/878a36b5-31af-4c36-86e6-5dbf432e9331/download/kved.json')
+    return response.json()
 
 
 def find(func, values):
@@ -21,14 +27,15 @@ def find(func, values):
             return value
 
 
-def parse_kved(classCode: str):
+def parse_kved(classCode: str, kved_obj):
     '''
     Parses kved tree and writes results to kved_result.json
     '''
     divisionCode = classCode.split('.')[0]
     groupCode = classCode[:-1]
 
-    kved_obj = read_file()
+    if not kved_obj:
+        kved_obj = get_kved_data()
     for section in kved_obj['sections'][0]:
         divisions = section['divisions']
         division = find(lambda i: i['divisionCode'] == divisionCode, divisions)
@@ -73,8 +80,18 @@ def parse_kved(classCode: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('kved', help='kved you want to find (example: 01.11)', type=str)
+    parser.add_argument('-s', '--source_file', help='Where your kved.json is located', type=str, action='store')
+    parser.add_argument('-d', '--destination_file',
+                        help='Where should the results be written to', type=str, action="store")
     args = parser.parse_args()
 
+    kved_obj = None
+    if args.s:
+        kved_obj = read_file(args.s)
+
+    destination_file = 'kved_result.json'
+    if args.d:
+        destination_file = args.d
     result = parse_kved(args.kved)
-    with open('kved_result.json', 'w') as file:
+    with open(destination_file, 'w') as file:
         json.dump(result, file, indent=2, ensure_ascii=False)
