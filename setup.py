832968365ad7e6f52016dd2a3022a6c8bea53c02
Pr0digg <impr0digg@gmail.com>
2021-02-21 17:11:05 +0200

    Trting to release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kved_finder",
-    version="v2.2",
+    version="v2.4",
     author="elem3ntary",
     author_email="impr0digg@gmai.com",
     description="A package for finding kved tree",
